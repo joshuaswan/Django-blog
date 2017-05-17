a6b5ad1246859b5ada900b65c89b1062bf100439
joshuaswan <joshuaswan@sina.com>
2017-05-17 18:18:11 +0800

    [joshua]成功了，主要原因是对应的数据格式有问题，对应posts对应一个数组，而这个数组直接传递给templates中的posts，从而进行循环显示。下班回家。

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,9 +5,6 @@ from . import models
 def archive(request):
     posts = models.BlogPost.objects.all()
     t = loader.get_template("archive.html")
-    for post in posts:
-        temp = [ post.title,post.timestamp,post.body]
-        print(temp)
-        data = {'posts': temp}
+    data = {'posts': posts}
     print(data)
     return HttpResponse(t.render(data))
